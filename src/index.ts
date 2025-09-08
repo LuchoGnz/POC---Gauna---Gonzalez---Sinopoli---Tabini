c702028a67d8d7be2a1ba9a12eeeb77fb438ae2f
LuchoGnz <luchogonzalez71@gmail.com>
2025-09-08 16:54:10 -0300

    Solucion de un par de errores

--- src/index.ts
+++ src/index.ts
@@ -26,7 +26,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.set("view engine", "ejs");
-app.set("views", __dirname + "/views");
+app.set("views", __dirname + "/../views");
 
 // ------------------ Rutas ------------------
 
